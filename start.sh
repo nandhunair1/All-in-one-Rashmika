e49c5d6039d4ba5e83bf85b645398c47ebc75a6d
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-11-26 22:33:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Localusersbot/Rashmika-The-Bot.git /Rashmika-The-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Rashmika-The-Bot
 fi
-cd /DQ-The-File-Donor
+cd /Rashmika-The-Bot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Rashmika-The-Bot...."
 python3 bot.py
